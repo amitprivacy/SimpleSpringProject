e6da2497eb55dc63a02e65e7279f5e8375dbebe9
Amit Dhapola <amitsingh43713@gmail.com>
2018-07-14 13:42:16 +0530

    New file added

--- src/org/springframework/chapter1/service/GreetingMessageServiceImpl.java
+++ src/org/springframework/chapter1/service/GreetingMessageServiceImpl.java
@@ -7,7 +7,7 @@ public class GreetingMessageServiceImpl implements GreetingMessageService {
 	
 	public String greetUser()
 	{
-		return "Welcome to Chapter-1 of book Learning Spring Application Development, Spring Book";
+		return "Great. You just completed First Chapter.";
 	}
 	
 	
